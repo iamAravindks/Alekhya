779a1f4bc4b83749d9154666a62d3ee98d6ebf99
jaison <jaisonjoshi2001@gmail.com>
2020-08-13 00:25:37 +0530

    cleared

--- js/main.js
+++
@@ -1,6 +0,0 @@
-var navbar = document.getElementsByClassName('navbar')[0];
-var toggler = document.getElementsByClassName('toggle-collapse')[0];
-
-toggler.addEventListener('click',function(){
-    navbar.classList.toggle('nav-collapse');
-})
