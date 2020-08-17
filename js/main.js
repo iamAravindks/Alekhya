b794565d1276a5e227ae17fc17b84ca93fc8c0b5
jaison <jaisonjoshi2001@gmail.com>
2020-08-17 15:03:51 +0530

    sariyayoo

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ var toggleIcon = document.querySelector('.toggle-collapse');
 toggleIcon.addEventListener('click', function(){
     navbar.classList.toggle('nav-collapse');
 });
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset>=100){
+        navbar.classList.add('nav-scroll');
+    }
+    else{
+        navbar.classList.remove('nav-scroll');
+    }
+})
