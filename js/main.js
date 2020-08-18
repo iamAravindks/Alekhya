7977b913fa40f51e17e69c89ed7c232d5f024493
jaison <jaisonjoshi2001@gmail.com>
2020-08-18 17:16:14 +0530

    aavo

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ toggleIcon.addEventListener('click', function(){
     navbar.classList.toggle('nav-collapse');
 });
 
+bgSite = document.querySelector('.bg-site');
+bgTop = bgSite.offsetTop;
 window.addEventListener('scroll', function(){
-    if(window.pageYOffset>=100){
+    if(window.scrollY>100){
         navbar.classList.add('nav-scroll');
     }
     else{
